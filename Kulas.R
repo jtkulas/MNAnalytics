8dfce55ad88952cef243997ebded21c97a7a8aff
Diego Figueiras <figueirasd1@montclair.edu>
2021-03-17 18:43:20 -0400

    Added extra algorithms, used the argument "verboseIter" to keep track of the iterations, and added knnunpute to handle NAs.

--- Kulas.R
+++ Kulas.R
@@ -13,7 +13,7 @@ hist(data$POSTSEASON)
 
 library(caret)
 
-control <- trainControl(method="cv", number=10)
+control <- trainControl(method="cv", number=10,verboseIter = TRUE)
 metric <- "RMSE"
 
 use <- data[complete.cases(data), ]                          # Problem with NAs (3/17/21)
@@ -22,12 +22,32 @@ use <- data[complete.cases(data), ]                          # Problem with NAs 
 ## Too many predictors, need to trim down
 
 # psych::describe(use)
-use2 <- use[,-c(1,3,24)]
-
-fit.lm <- train(POSTSEASON ~ ., data=use2, method="lm", metric=metric, trControl=control)
-fit.wm <- train(POSTSEASON ~ ., data=use2, method="WM", metric=metric, trControl=control)
+use2 <- use[,-c(1,2,3,24)]
+set.seed(32)
+fit.lm <- train(POSTSEASON ~ ., data=use2, method="lm", metric=metric, trControl=control, preProcess = "knnImpute")
+set.seed(56)
+fit.wm <- train(POSTSEASON ~ ., data=use2, method="WM", metric=metric, trControl=control, preProcess = "knnImpute")
 # fit.evtree <- train(POSTSEASON ~ ., data=use2, method="evtree", metric=metric, trControl=control)
 
+# CART
+set.seed(90)
+fit.cart <- train(POSTSEASON~., data=use2, method="rpart", metric=metric, trControl=control, preProcess = "knnImpute")
+# kNN
+set.seed(12)
+fit.knn <- train(POSTSEASON~., data=use2, method="knn", metric=metric, trControl=control, preProcess = "knnImpute")
+# c) advanced algorithms
+# SVM
+set.seed(48)
+fit.svm <- train(POSTSEASON~., data=use2, method="svmRadial", metric=metric, trControl=control, preProcess = "knnImpute")
+# Random Forest
+set.seed(87)
+fit.rf <- train(POSTSEASON~., data=use2, method="rf", metric=metric, trControl=control, preProcess = "knnImpute")
+
+
+# summarize accuracy of models
+results <- resamples(list(cart=fit.cart, knn=fit.knn, svm=fit.svm, rf=fit.rf, lm=fit.lm, wm=fit.wm))
+summary(results)
+dotplot(results)
 results <- resamples(list(lm=fit.lm, wm=fit.wm))
 summary(results)
 dotplot(results)
@@ -36,5 +56,6 @@ dotplot(results)
 validate <- read.csv("cbb21.csv")
 validate <- validate[,-c(1,3)]
 
-predictions <- predict(fit.lm, validate)
+predictions <- predict(fit.wm, validate)
 write.csv(predictions, "winner.csv")
+
