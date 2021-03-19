115f36547f664d67ca002df847144315d76359a7
John-o Kulas <kulasj@montclair.edu>
2021-03-19 11:07:42 -0400

    Add files via upload

--- gamelogs.R
+++ gamelogs.R
@@ -62,57 +62,38 @@ game2018 <- subset(both, Season == 2018)
 game2019 <- subset(both, Season == 2019)
 
 to_2010 <- merge(game2010, twenty10, by.x = "Winner", by.y = "X.1")   ## Adding winning team predictors
-to_2011 <- merge(game2011, twenty11, by.x = "Winner", by.y = "X.1")
 to_2012 <- merge(game2012, twenty12, by.x = "Winner", by.y = "X.1")
-to_2013 <- merge(game2013, twenty13, by.x = "Winner", by.y = "X.1")
 to_2014 <- merge(game2014, twenty14, by.x = "Winner", by.y = "X.1")
-to_2015 <- merge(game2015, twenty15, by.x = "Winner", by.y = "X.1")
 to_2016 <- merge(game2016, twenty16, by.x = "Winner", by.y = "X.1")
-to_2017 <- merge(game2017, twenty17, by.x = "Winner", by.y = "X.1")
 to_2018 <- merge(game2018, twenty18, by.x = "Winner", by.y = "X.1")
-to_2019 <- merge(game2019, twenty19, by.x = "Winner", by.y = "X.1")
+
 
 bo_2010 <- merge(to_2010, twenty10, by.x = "Loser", by.y = "X.1")     ## Adding losing team predictors
-bo_2011 <- merge(to_2011, twenty11, by.x = "Loser", by.y = "X.1")
 bo_2012 <- merge(to_2012, twenty12, by.x = "Loser", by.y = "X.1")
-bo_2013 <- merge(to_2013, twenty13, by.x = "Loser", by.y = "X.1")
 bo_2014 <- merge(to_2014, twenty14, by.x = "Loser", by.y = "X.1")
-bo_2015 <- merge(to_2015, twenty15, by.x = "Loser", by.y = "X.1")
 bo_2016 <- merge(to_2016, twenty16, by.x = "Loser", by.y = "X.1")
-bo_2017 <- merge(to_2017, twenty17, by.x = "Loser", by.y = "X.1")
 bo_2018 <- merge(to_2018, twenty18, by.x = "Loser", by.y = "X.1")
-bo_2019 <- merge(to_2019, twenty19, by.x = "Loser", by.y = "X.1")
 
-big_predictor_win <- rbind(bo_2010, bo_2011, bo_2012, bo_2013, bo_2014, bo_2015, bo_2016, bo_2017, bo_2018, bo_2019)
+big_predictor_win <- rbind(bo_2010, bo_2012, bo_2014, bo_2016, bo_2018)
 
 big_predictor_win$DV1 <- 1
 big_predictor_win$DV3 <- big_predictor_win$WScore - big_predictor_win$LScore
   
 ## need to duplicate for loser but swap winner and losers
 
-toq_2010 <- merge(game2010, twenty10, by.x = "Loser", by.y = "X.1")   ## Adding winning team predictors
 toq_2011 <- merge(game2011, twenty11, by.x = "Loser", by.y = "X.1")
-toq_2012 <- merge(game2012, twenty12, by.x = "Loser", by.y = "X.1")
 toq_2013 <- merge(game2013, twenty13, by.x = "Loser", by.y = "X.1")
-toq_2014 <- merge(game2014, twenty14, by.x = "Loser", by.y = "X.1")
 toq_2015 <- merge(game2015, twenty15, by.x = "Loser", by.y = "X.1")
-toq_2016 <- merge(game2016, twenty16, by.x = "Loser", by.y = "X.1")
 toq_2017 <- merge(game2017, twenty17, by.x = "Loser", by.y = "X.1")
-toq_2018 <- merge(game2018, twenty18, by.x = "Loser", by.y = "X.1")
 toq_2019 <- merge(game2019, twenty19, by.x = "Loser", by.y = "X.1")
 
-boq_2010 <- merge(to_2010, twenty10, by.x = "Winner", by.y = "X.1")     ## Adding losing team predictors
-boq_2011 <- merge(to_2011, twenty11, by.x = "Winner", by.y = "X.1")
-boq_2012 <- merge(to_2012, twenty12, by.x = "Winner", by.y = "X.1")
-boq_2013 <- merge(to_2013, twenty13, by.x = "Winner", by.y = "X.1")
-boq_2014 <- merge(to_2014, twenty14, by.x = "Winner", by.y = "X.1")
-boq_2015 <- merge(to_2015, twenty15, by.x = "Winner", by.y = "X.1")
-boq_2016 <- merge(to_2016, twenty16, by.x = "Winner", by.y = "X.1")
-boq_2017 <- merge(to_2017, twenty17, by.x = "Winner", by.y = "X.1")
-boq_2018 <- merge(to_2018, twenty18, by.x = "Winner", by.y = "X.1")
-boq_2019 <- merge(to_2019, twenty19, by.x = "Winner", by.y = "X.1")
+boq_2011 <- merge(toq_2011, twenty11, by.x = "Winner", by.y = "X.1")
+boq_2013 <- merge(toq_2013, twenty13, by.x = "Winner", by.y = "X.1")
+boq_2015 <- merge(toq_2015, twenty15, by.x = "Winner", by.y = "X.1")
+boq_2017 <- merge(toq_2017, twenty17, by.x = "Winner", by.y = "X.1")
+boq_2019 <- merge(toq_2019, twenty19, by.x = "Winner", by.y = "X.1")
 
-big_predictor_lose <- rbind(boq_2010, boq_2011, boq_2012, boq_2013, boq_2014, boq_2015, boq_2016, boq_2017, boq_2018, boq_2019)
+big_predictor_lose <- rbind(boq_2011, boq_2013, boq_2015, boq_2017, boq_2019)
 
 big_predictor_lose$DV1 <- 0
 big_predictor_lose$DV3 <- big_predictor_lose$LScore - big_predictor_lose$WScore
@@ -145,7 +126,7 @@ memory.limit(16500)
 fit.lm <- train(DV3 ~ ., data=user, method="lm", metric=metric, trControl=control)
 # summary(fit.lm)
 
-write.csv(game2010, "template.csv")
+## write.csv(game2010, "template.csv")
 
 
 current <- read.csv("current_bracket.csv")
@@ -169,3 +150,36 @@ predictions <- predict(fit.lm, validate)
 write.csv(predictions, "winnerR1.csv")
 
 
+####
+####
+#### Logistic regression
+
+## Need to swap DV3 with DV1
+
+finally <- rbind(big_predictor_win, big_predictor_lose)
+finally <- finally[,-c(27,28,31,32,35,36,71,72,75,76)] ## data not completely good but tourney starts manana
+finally2 <- finally[,c(6,14:19,25:68)]
+dat2 <- data.frame(lapply(finally2, function(x) as.numeric(as.character(x))))
+
+user <- dat2[complete.cases(dat2), ]
+
+
+trControl2 <- trainControl(method = 'repeatedcv',
+                          number = 5,
+                          repeats =  5,
+                          search = 'random')
+
+logit.CV <- train(DV1 ~ ., data=user, 
+                  method = 'glmnet',
+                  trControl = trControl2,
+                  family = 'binomial' )
+
+
+user$DV1 <- as.factor(as.character(user$DV1))
+logit.CV2 <- train(DV1 ~ ., data=user, 
+                  method = 'glmnet',
+                  trControl = trControl2,
+                  family = 'binomial' )
+
+predictions2 <- predict(logit.CV2, validate)
+write.csv(predictions2, "winnerLOGISTIC.csv")
